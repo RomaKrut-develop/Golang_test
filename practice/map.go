3b856043527af39b8f34350d51e9d9771e7ff449
RomaKrut-develop <romanmaksbetiv228@gmail.com>
2025-11-11 13:20:49 +0300

    Changed and added new practice

--- practice/map.go
+++ practice/map.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	strToNum := map[string]int{
+	strToNum := map[string]int{ // Словарь
 		"zero": 0,
 		"one": 1,
 		"two": 2,
 		"three": 3,
 	}
 
-	value, ok := strToNum["zero"]
-
-	fmt.Println(value, ok)
+	if value, ok := strToNum["zero"]; ok { // В услвоиях мы также можем присавивать значения аргументам
+		fmt.Println("Zero is inside map and it's value is", value)
+	} else {
+		fmt.Println("Zero is missing")
+	}
 }
